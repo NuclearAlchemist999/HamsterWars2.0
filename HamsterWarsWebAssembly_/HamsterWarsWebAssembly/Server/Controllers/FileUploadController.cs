33eaa84b945f2cbda41dff07444535cafec50b45
Rebecca <lobson999@protonmail.com>
2022-06-04 14:58:29 +0200

    Added file upload validation in backend and improved file upload in frontend

--- HamsterWarsWebAssembly_/HamsterWarsWebAssembly/Server/Controllers/FileUploadController.cs
+++ HamsterWarsWebAssembly_/HamsterWarsWebAssembly/Server/Controllers/FileUploadController.cs
@@ -1,4 +1,5 @@
 ﻿using HamsterWarsWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
@@ -19,11 +20,29 @@ namespace HamsterWarsWebAssembly.Server.Controllers
         [HttpPost]
         public async Task<IActionResult> PostFile(UploadedFile uploadedFile)
         {
+            string[] validTypes = { "jpg", "jpeg", "png" };
+
+            string fileExtension = uploadedFile.FileName.Split(".").Last();
+
+            if (string.IsNullOrEmpty(fileExtension) || !validTypes.Contains(fileExtension.ToLower()))
+            {
+                return BadRequest("The file does not have an extension or it is not an image.");
+            }
+
+            if(uploadedFile.FileContent.Length > 512000)
+            {
+                return BadRequest("The size of file is more than 512 kb.");
+            }
+
+
             var path = $"wwwroot/images/{uploadedFile.FileName}";
             await using var fs = new FileStream(path, FileMode.Create);
             fs.Write(uploadedFile.FileContent, 0, uploadedFile.FileContent.Length);
             return new CreatedResult(_env.WebRootPath, uploadedFile.FileName);
+
+            
         }
+
     }
 
 }
--- HamsterWarsWebAssembly_/HamsterWarsWebAssembly/Shared/Models/Hamster.cs
+++ HamsterWarsWebAssembly_/HamsterWarsWebAssembly/Shared/Models/Hamster.cs
@@ -8,21 +8,21 @@ namespace HamsterWarsWebAssembly.Shared.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Enter a name!")]
+        [Required(ErrorMessage = "Enter a name.")]
         [MinLength(2, ErrorMessage = "At least two characters.")]
         public string? Name { get; set; }
 
         [Range(1, 5, ErrorMessage = "Age must be between 1 and 5.")]
         public int Age { get; set; }
 
-        [Required(ErrorMessage = "Enter favourite food!")]
+        [Required(ErrorMessage = "Enter favourite food.")]
         [MinLength(3, ErrorMessage = "At least three characters.")]
         public string? FavFood { get; set; }
 
-        [Required(ErrorMessage = "Enter favourite thing!")]
+        [Required(ErrorMessage = "Enter favourite thing.")]
         [MinLength(3, ErrorMessage = "At least three characters.")]
         public string? FavThing { get; set; }
-        [Required(ErrorMessage = "Enter an image name!")]
+        [Required(ErrorMessage = "An image is required.")]
         [MinLength(5, ErrorMessage = "At least five characters.")]
         public string? ImgName { get; set; }
         [Range(0, 0, ErrorMessage = "Zero by default.")]
